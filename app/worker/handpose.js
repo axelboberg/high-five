a08d749d7a8e14fef10ddc523ebd0ea5031e3ef1
Axel Boberg <ovrigt@axelboberg.se>
2020-04-15 23:23:42 +0200

    Remove unused code

--- app/worker/handpose.js
+++ app/worker/handpose.js
@@ -3,20 +3,6 @@
  */
 
 const Handpose = require('@tensorflow-models/handpose')
-// const tf = require('@tensorflow/tfjs')
-
-/* const detect = require('./detect')
-const model = require('./handopen.detect') */
-
-/* console.log(tf.getBackend())
-
-tf.setBackend('webgl')
-  .then(res => {
-    console.log(res)
-    console.log(tf.getBackend())
-  })
-  .catch(err => console.error(err)) */
-
 let model 
 
 exports.predict = async (image, width, height) => {
@@ -36,45 +22,6 @@ exports.predict = async (image, width, height) => {
   const prediction = await model.estimateHands(canvas, false)
   return parse(prediction[0], width, height)
 }
-
-/* exports.predict = async (image, width, height) => {
-  if (!detector) {
-    detector = new detect.detector(width, height, 1.1, model)
-  }
-
-  const canvas = new OffscreenCanvas(width, height)
-  const ctx = canvas.getContext('2d')
-        ctx.drawImage(image, 0, 0)
-
-  return detector.detect(canvas, 1)[0]
-} */
-
-/* exports.predict = async (image, width, height) => {
-  const opts = {
-    detectionConfidence: 0.95
-  }
-
-  if (!model) {
-    model = await tf.loadLayersModel('/public/model/model.json') */
-/*     try {
-      model = await tf.loadLayersModel('/public/model/model.json')
-    } catch (_) {
-      model = await Handpose.load(opts)
-              await model.save('indexeddb://handpose')
-    } */
-/*   }
-
-  const canvas = new OffscreenCanvas(width, height)
-  const ctx = canvas.getContext('2d')
-        ctx.drawImage(image, 0, 0)
-
-  let data = tf.browser.fromPixels(canvas)
-      data = tf.reshape(data, [ -1,416,416,3 ])
-      data = tf.cast(data, 'float32')
-
-  const prediction = await model.predict(data)
-  return prediction
-} */
 
 function parse (data, width, height) {
   if (!data) return
